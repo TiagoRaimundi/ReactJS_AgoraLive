0675ee7c9fb307a2676540a153fc76530a8d230b
Tiago Raimundi <raimunditiagoaugusto@gmail.com>
2023-10-02 18:11:27 -0300

    jose

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   const rtcProps = {
     appId: "058da73f6dec4619b8ffef8b4b104e61",
     channel: "LiveAgora",
+    token: "007eJxTYJBdVbTh/mVZ3n36d2OFOPaduaN4MY3t6CeJP6LTLJ2lDTYoMBiYWqQkmhunmaWkJpuYGVomWaSlpaZZJJkkGRqYpJoZGupIpzYEMjJkq4WxMDJAIIjPyeCTWZbqmJ5flMjAAACMlR8V",
  
   };
 
